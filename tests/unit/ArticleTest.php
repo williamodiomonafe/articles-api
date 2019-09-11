68af2928aaedea37d6028ec335f858011bc3f11c
William Odiomonafe <william.odiomonafe@gmail.com>
2019-09-11 13:04:29 +0100

    Initial Commit

---
+++ tests/unit/ArticleTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Oshoname William Odiomonafe.
+ * User: Oshoname William Odiomonafe
+ * Date: 9/11/2019
+ * Time: 12:52 PM
+ */
+
+use App\Article;
+
+class ArticleTest extends TestCase {
+    public function testResponseFromArticlePost() {
+
+    }
+}
